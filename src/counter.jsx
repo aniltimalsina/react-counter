81ebd5a45755bf068a227a90e3e2ef0a4e5ec24e
aniltimalsina <timalsinaanil96@gmail.com>
2023-09-19 00:11:46 -0400

    add counter component with logic

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,10 +1,28 @@
 import "./counter.css";
-import React, { useState } from "react";
+import { useState } from "react";
 
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
     <>
-      <div></div>
+      <div className="container">
+        <button onClick={increment} className="sign">
+          Increment
+        </button>
+        <h1>Counter: {count}</h1>
+
+        <button onClick={decrement} className="sign">
+          Decrement
+        </button>
+      </div>
     </>
   );
 }
